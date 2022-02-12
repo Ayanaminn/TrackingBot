9fff199d506b5324bd74aa5802c66687f6a743cf
Yutao <s3593366@student.rmit.edu.au>
2022-02-12 16:09:32 +1100

    first commit

---
+++ TrackingBot/video_player.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# TrackingBot - A software for video-based animal behavioral tracking and analysis
+# Developer: Yutao Bai <hitomiona@gmail.com>
+# https://www.neurotoxlab.com
+
+# Copyright (C) 2022 Yutao Bai
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from PyQt5.QtCore import pyqtSignal, QThread,QObject,QMutex,QMutexLocker
+import time
+
+
+class VideoThread(QThread):
+
+    def __init__(self, default_fps=25):
+        QThread.__init__(self)
+        self.stopped = False
+        self.fps = default_fps
+        self.timeSignal = Communicate()
+        self.mutex = QMutex()
+
+    def run(self):
+        with QMutexLocker(self.mutex):
+            self.stopped = False
+
+        while True:
+            if self.stopped:
+                return
+            self.timeSignal.signal.emit('1')
+            time.sleep(1 / self.fps)
+
+    def stop(self):
+        with QMutexLocker(self.mutex):
+            self.stopped = True
+
+    def is_stopped(self):
+        with QMutexLocker(self.mutex):
+            return self.stopped
+
+    def set_fps(self, video_fps):
+        self.fps = video_fps
+
+
+class Communicate(QObject):
+    signal = pyqtSignal(str)
+
+
